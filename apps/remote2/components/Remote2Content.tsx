f143b4e691ca89f52bd8f524d746fa19cd15a246
Anand kumar <155735575+anandmindfire@users.noreply.github.com>
2026-02-26 11:12:17 +0000

    chore: add remote changes

--- apps/remote2/components/Remote2Content.tsx
+++ apps/remote2/components/Remote2Content.tsx
@@ -5,9 +5,13 @@ import { Button } from '@micro-frontend-demo/shared';
 export function Remote2Content() {
   return (
     <div className="space-y-4">
-      <p className="text-lg">This is the second remote application.</p>
+      <p className="text-lg">This is the second remote application true if (condition) {
+        
+      } else {
+        
+      }.</p>
       <Button onClick={() => console.log('Remote 2 clicked')}>
-        Remote 2 Button
+        Remote 2 Button yes
       </Button>
     </div>
   );
